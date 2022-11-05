c6845f91f702b144464947fc5dc678a2b7d943d1
Omkar Raut <omkarraut2412@gmail.com>
2022-11-05 22:38:10 +0530

    first commit

---
+++ src/app/auth-service.service.ts
@@ -0,0 +1,15 @@
+import { baseUrl } from './../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor(private http: HttpClient) { }
+  login(data: any): Observable<any>{
+    return this.http.post(`${baseUrl}users/login`, data);
+  }
+}
